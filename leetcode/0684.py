ee1bfc1f0cb0db8e5fed405a2deb842deb575522
regius <gilgemesh2012@gmail.com>
2024-04-09 14:07:52 +0800

    update 13

--- leetcode/0684.py
+++ leetcode/0684.py
@@ -1,29 +1,29 @@
-from collections import defaultdict
 from typing import List
 
 
+class DisjointSet:
+
+    def __init__(self, n: int):
+        self.root = [x for x in range(n)]
+
+    def find(self, x: int):
+        if self.root[x] == x:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def merge(self, x: int, y: int):
+        x, y = self.find(x), self.find(y)
+        if x != y:
+            self.root[x] = y
+
+
 class Solution:
-    def __init__(self):
-        self.hasCycle = False
-
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        graph = defaultdict(list)
-
-        def dfs(u: int, f: int):
-            visit[u] = True
-            for v in graph[u]:
-                if v == f:
-                    continue
-                if not visit.get(v, False):
-                    dfs(v, u)
-                else:
-                    self.hasCycle = True
+        n = len(edges)
+        ds = DisjointSet(n + 1)
 
         for u, v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
-            self.hasCycle = False
-            visit = {}
-            dfs(u, 0)
-            if self.hasCycle:
+            if ds.find(u) == ds.find(v):
                 return [u, v]
+            ds.merge(u, v)
